abd03d778b9b0f3668ab3e603a111974cbafa644
Ali Nuraldin <alirunwwc@gmail.com>
2020-01-16 10:30:53 +0100

    Change HasCommission properties visibility

--- contracts/Helpers/HasCommission.sol
+++ contracts/Helpers/HasCommission.sol
@@ -3,9 +3,9 @@ pragma solidity 0.5.16;
 /// @title Opium.Helpers.HasCommission contract helps to syntheticId development and responsible for commission and author address
 contract HasCommission {
     // Address of syntheticId author
-    address public author;
+    address internal author;
     // Commission is in Opium.Lib.LibCommission.COMMISSION_BASE base
-    uint256 constant public AUTHOR_COMMISSION = 25; // 0.25% of profit
+    uint256 constant internal AUTHOR_COMMISSION = 25; // 0.25% of profit
 
     /// @notice Sets `msg.sender` as syntheticId author
     constructor() public {
